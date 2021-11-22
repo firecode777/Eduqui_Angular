ef86eab2bb249d3406d2bd7cc4d96d857ef9cfa6
HenriQueMeF <melhadohenrique@gmail.com>
2021-11-22 14:39:40 -0300

    Segurança melhorada

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,11 @@
+require('dotenv').config()
 const express = require ('express');
 const app = express();
 
-mongoose.connect('mongodb+srv://eduqui:eduqui123@eduqui.s90nm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@eduqui.${process.env.DB_ADDRESS}.mongodb.net/${process.env.DB_DATABASE}?retryWrites=true&w=majority`)
 .then(() =>  console.log ("CONEXAO OK"))
 .catch (e => console.log ("CONEXAO NOK: " + e))
 
-app.use('/api/forum', (req, res, next) => {
-  res.status(200).json({
-      mensagem:"Tudo OK",
-      forum: forum
-    }
-  );
-});
+app.use('/api/forum', );
 
 module.exports = app;
--- src/app/verificar-email/verificar-email.component.ts
+++ src/app/verificar-email/verificar-email.component.ts
@@ -10,6 +10,7 @@ export class VerificarEmailComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
 }
